f7be7a21fe191f9057d118d5a237765654b31f27
Samin Semsar <anonymous.researcher.25@gmail.com>
2024-07-16 23:39:28 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/*const express = require('express');
+const app = express();
+const port = 3000
+
+app.use('/', express.static(__dirname + '/'));
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});*/
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
