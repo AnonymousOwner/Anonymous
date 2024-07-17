59f116dd795a399f51acb21f0f330f343fb1dedd
Samin Semsar <anonymous.researcher.25@gmail.com>
2024-07-17 08:14:47 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ const app = express();
 const port = 3000;
 
 // Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 app.listen(port, () => {
